c21f4495921766e6e78aed245cb9fdb261445bbc
Ellen Schrader <mail@ellen-schrader.de>
2025-01-17 21:44:49 +0000

    classify cancer type conch

---
+++ conch_model/utils/data.py
@@ -0,0 +1,12 @@
+import openslide
+import PIL.Image
+
+def read_slide(img, coords = (0,0), dim=(512, 512), level=1):
+    slide = openslide.open_slide(img)
+    width, height = slide.dimensions
+    image = slide.read_region(coords, level, dim)
+    
+    return image
+
+def read_img(img):
+    return PIL.Image.open(img)
