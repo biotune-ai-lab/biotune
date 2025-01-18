1b96e903aef59e171936c7c9062c3a899df27b00
ishgod01 <ishan.godawatta@arm.com>
2025-01-18 00:22:30 +0000

    Updating broken imports

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 import { useUpload } from "../utilities/runtime-helpers";
